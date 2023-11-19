061bffef6b492f2e89901183b6ec60e0e5746266
tirtafarisaldi <tirtafarisaldi@gmail.com>
2023-11-19 20:53:58 +0700

    init

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import { ping } from './services'
+import { show } from './views/message'
+
+const supportedAPI = ['key']; // enlist all methods supported by API (e.g. `mw('event', 'user-login');`)
+
+function app(window) {
+
+    // set default configurations
+    let configurations = {
+        someDefaultConfiguration: false
+    };
+
+    // all methods that were called till now and stored in queue
+    // needs to be called now 
+    let globalObject = window[window['JS-SCRIPT']];
+    let queue = globalObject.q;
+
+    // override temporary (until the app loaded) handler
+    // for widget's API calls
+    globalObject = apiHandler(queue[0][0], queue[0][1]);
+    // globalObject.configurations = configurations;
+}
+
+/**
+    Method that handles all API calls
+    */
+function apiHandler(api, params) {
+    if (!api) throw Error('API method required');
+    api = api.toLowerCase();
+
+    if (supportedAPI.indexOf(api) === -1) throw Error(`Method ${api} is not supported`);
+
+    console.log(`Handling API call ${api}`, params);
+
+    switch (api) {
+        // TODO: add API implementation
+        case 'key':
+            show(params);
+            break;
+        default:
+            console.warn(`No handler defined for ${api}`);
+    }
+}
+
+function extendObject(a, b) {
+    for (var key in b)
+        if (b.hasOwnProperty(key))
+            a[key] = b[key];
+    return a;
+}
+
+app(window);
